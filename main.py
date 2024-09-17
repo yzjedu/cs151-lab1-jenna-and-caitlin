6d14b22fc80b6ff76ed4fbf3cf78afe0764099f5
Jenna and Caitlin <jmcuti@loyola.edu>
2024-09-17 16:52:24 -0400

    #programmers : Caitlin and Jenna #course: CS151 Dr. Zee Yalew #9/18/24 #lab assignment 1 #problem statement calculates miles traveled, miles per gallon, and cost of gas to output price per gallon #Data in: Input requires miles, miles per gallon, gas price #Data out: Total gas price #credits: code is based off a class example

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-# Enter code here
 # Read the README for instructions.
 # You must write an algorithm and test cases first!
+#this program calculates and total gas price
+#prompt the use to enter miles driven
+
+Miles_str=input("Enter Miles: ")
+miles_int=int(Miles_str)
+
+#prompt the user to enter MPG
+MPG_str=input("Enter MPG: ")
+MPG_int=int(MPG_str)
+
+#prom-pt the user to enter PPG
+PPG_str=input("Enter PPG: ")
+PPG_int=int(PPG_str)
+
+#calculate miles per gallon
+
+Cost=miles_int/MPG_int*PPG_int
+
+#display cost
+print("Your cost per gallon is:" , Cost)
